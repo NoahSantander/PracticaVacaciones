343e0f311495ae58cf0cf05456722fb9cba80fc2
Noah <noahsantander@gmail.com>
2023-05-30 19:11:41 -0300

    Completado el parcial

--- src/Library.hs
+++ src/Library.hs
@@ -8,6 +8,8 @@ type Idioma = String
 type Minutos = Number
 type Marea = String
 type Indice = Turista -> Number
+type Tour = [Excursion]
+type Espiritualidad = Number
 type Excursion = Turista -> Turista -- Modelar para agregar mas funciones sin cambiar las existentes
 
 --Defino mis turistas
@@ -27,6 +29,18 @@ agregarIdioma idioma idiomas
     |estaIdioma idioma idiomas = idiomas
     |otherwise = idioma:idiomas
 
+subirStress :: Turista -> Number -> Turista
+subirStress turista stress = turista{nivelStress = nivelStress turista + stress}
+
+estaAcompañado :: Turista -> Bool
+estaAcompañado turista = viajaSolo turista
+
+obtenerCansancio :: Turista -> Cansancio
+obtenerCansancio turista = nivelCansancio turista
+
+obtenerStress :: Turista -> Stress
+obtenerStress turista = nivelStress turista
+
 --Defino mis excursiones
 irPlaya :: Excursion
 irPlaya turista 
@@ -45,6 +59,38 @@ caminar minutos turista = turista {nivelCansancio = nivelCansancio turista + min
 paseoBarco :: Marea -> Excursion
 paseoBarco "Fuerte" turista = turista {nivelCansancio = nivelCansancio turista + 10, nivelStress = (nivelStress turista + 6)*0.9}
 paseoBarco "Tranquila" turista = hablarIdioma "Aleman" (apreciarPaisaje "mar" (caminar 10 turista))
+
+--Defino mis tours
+completo :: Tour
+completo = [caminar 20, apreciarPaisaje "cascada", caminar 40, irPlaya , hablarIdioma "melmacquiano"]
+
+ladoB :: Excursion -> Tour --Cuando a ladoB le doy una excursion, me devuelve un Tour, es decir, [Excursion]
+ladoB excursion = [paseoBarco "Tranquila", excursion, caminar 120] --De la misma manera funciona paseoBarco, ya que al darle "Tranquila" devuelve una Excursion
+
+islaVecina :: Marea -> Tour
+islaVecina "Fuerte" = [paseoBarco "Fuerte", apreciarPaisaje "cascada", paseoBarco "Fuerte"]
+islaVecina marea = [paseoBarco marea, irPlaya, paseoBarco marea]
+
+hacerExcursion :: Turista -> Excursion -> Turista
+hacerExcursion turista excursion = excursion turista
+
+hacerTurismo :: Turista -> Tour -> Turista
+hacerTurismo turista tour = foldl (hacerExcursion) turista tour
+
+esConvincente :: Turista -> Indice -> Tour -> Bool
+esConvincente turista indice tour = elem True (excursionesDesestresantes tour indice turista) && not(estaAcompañado turista)
+
+convenceTurista :: [Tour] -> Indice -> Turista ->  Bool
+convenceTurista tours indice turista = elem True (map (esConvincente turista indice) tours) 
+
+obtenerConvencidos :: [Tour] -> [Turista] -> [Turista]
+obtenerConvencidos _ [] = []
+obtenerConvencidos tour (x:xs) 
+    |convenceTurista tour stress x = x:obtenerConvencidos tour xs
+    |otherwise = obtenerConvencidos tour xs
+
+playaInfinita :: Tour 
+playaInfinita = irPlaya:playaInfinita 
 
 --Defino mis indices
 stress :: Indice
@@ -77,7 +123,7 @@ cathi = UnTurista 15 15 True ["Aleman", "Catalan"]
 --Excursiones desestresantes
 esDesestresante :: Indice -> Turista -> Excursion -> Bool
 esDesestresante indice turista excursion
-    |deltaExcursionSegun indice turista excursion < -3 = True
+    |deltaExcursionSegun indice turista excursion < -2 = True
     |otherwise = False
 
 excursionesDesestresantes :: [Excursion] -> Indice -> Turista -> [Bool]
